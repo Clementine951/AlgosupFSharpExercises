229403cd337208e70d258b770f2acfa370a4b2bf
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-01-25 16:02:55 +0100

    arzezfrdfgb
    ezarfg

--- WebDev/src/Client/Search.fs
+++ WebDev/src/Client/Search.fs
@@ -17,6 +17,7 @@ let view (model: Model) (dispatch: Msg -> unit) =
                             prop.value model.Input
                             prop.placeholder "What are you looking for?"
                             prop.onChange (fun x -> SetInput x |> dispatch)
+                            prop.style  [style.width 980]
                         ]
                     ]
                 ]
@@ -36,21 +37,22 @@ let view (model: Model) (dispatch: Msg -> unit) =
         Bulma.control.p [
         for item in model.Items do
             if item.Images.IsEmpty |> not then
-                prop.children[
-                    Bulma.field.div [
-                        prop.children [
-                            Html.img [ prop.src (List.head item.Images); prop.style [style.height 300] ]
-                            Html.p [ prop.text item.Name ]
-                            Html.button [ 
-                                color.isPrimary
-                                // prop.onClick (fun _ -> dispatch Search)
-                                prop.text "Add to basket"
-                            ]
-                            Html.p [ prop.text $"Price: {item.Price} €" ]
-                            // Html.p [ prop.text item.Description ]
+                Bulma.field.div [
+                    prop.children [
+                        Html.img [ prop.src (List.head item.Images); prop.style [style.height 300] ]
+                        Html.p [ prop.text item.Name ]
+                        Html.button [ 
+                            color.isPrimary
+                            // prop.onClick (fun _ -> dispatch Search)
+                            prop.text "Add to basket"
                         ]
-                        prop.style [style.width 300]
+                        Html.p [ prop.text $"Price: {item.Price} €" ]
+                        // Html.p [ prop.text item.Description ]
                     ]
+                    prop.style [style.width 300]
                 ]
+                
         ]
-   ]
+        
+    ] 
+    prop.style [style.display inline]
