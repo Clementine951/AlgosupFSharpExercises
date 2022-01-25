91bd44f1209d8e4c6b1cbea9915e929c02e846d0
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2022-01-25 14:39:52 +0100

    Update Search.fs

--- WebDev/src/Client/Search.fs
+++ WebDev/src/Client/Search.fs
@@ -33,9 +33,9 @@ let view (model: Model) (dispatch: Msg -> unit) =
         Bulma.control.p [
         for item in model.Items do
             if item.Images.IsEmpty |> not then
-                Html.img [ prop.src (List.head item.Images); prop.style [style.height 400] ]
-            Html.p [ prop.text item.Name ]
-            Html.p [ prop.text item.Description ]
+                Html.img [ prop.src (List.head item.Images); prop.style [style.height 300] ]
+            // Html.p [ prop.text item.Name ]
             Html.button [ prop.text "Add to basket" ]
+            // Html.p [ prop.text item.Description ]
         ]
    ]
