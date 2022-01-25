8be5cea6d381ed0c7a3d201ae8ea7baefb7413c0
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2022-01-25 14:12:53 +0100

    Update Items.fs

--- WebDev/src/Server/Items.fs
+++ WebDev/src/Server/Items.fs
@@ -10,5 +10,24 @@ let itemList =
           Description = "Chic look! Practical, convenient, easy to use. Excellent sound quality."
           Price = 29M
           Images = [ "/items/RetroPhone.png" ] }
+
+        { Id = Guid.NewGuid()
+          Name = "Blond doll"
+          Description = "Cuddly toy for babies."
+          Price = 12M
+          Images = [ "/items/doll.jpg" ] }
+
+        { Id = Guid.NewGuid()
+          Name = "Jar Jar doll"
+          Description = "Star Wars – Edition Collector – Figurine Black Series Jar Jar Binks."
+          Price = 2067M
+          Images = [ "/items/jarjar.webp" ] }
+        
+        { Id = Guid.NewGuid()
+          Name = "MY little poney doll"
+          Description = "My Little Pony Friendship is Magic Princess Twilight Sparkle Cuddly Plush."
+          Price = 876M
+          Images = [ "/items/poney.jpg" ] }
+        
     ]
 
